b12b6574105b303f0a430287eff5b67819b04462
Raj <raj.onkar@gmail.com>
2026-04-21 16:27:23 +0530

    commit

--- outlier_identification/python_files/outlier_identification.py
+++ outlier_identification/python_files/outlier_identification.py
@@ -214,6 +214,9 @@ def generate_outlier_summary(df):
 # summary_report = generate_outlier_summary(df_final)
 # print(summary_report.head(10)) # Top 10 series with most anomalies
 
+print("printing outliers",df_final[df_final['series_id'] == 'Product_001'],'Product_001').head(20)
 
+# plot_outlier_results(df_final[df_final['series_id'] == 'Product_001'], 'Product_001')
+# summary_report = generate_outlier_summary(df_final)
+# print(summary_report.head(10))
 
-plot_outlier_results(df_final[df_final['series_id'] == 'Product_001'], 'Product_001')
