d5ecddbde63d478cc7c6e12e0a57f9d3ed579690
Raj <raj.onkar@gmail.com>
2026-06-04 21:09:39 +0530

    m

--- outlier_identification/python_files/prod_ready_outlier_identification_v5.py
+++ outlier_identification/python_files/prod_ready_outlier_identification_v5.py
@@ -126,7 +126,7 @@ print(df_final['promo_type'].unique())
 # the df_final has cols: series_id, week_start, sales, week_end, promo_type.
 # promo_type has values like Normal, BOGO, Event1, Event2, BOGO + Event1 etc. 
 # detect_outliers_n_segment function expects a certains col name dfu|sales|period if differnt col name change the col_name  plus one more column with name promo__type 
-# note that promo_type column name column is critical other cols can be renamed using the rename_map argument in the function detect_outliers_n_segment
+# note that promo_type column name column is CRITICAL other cols can be renamed using the rename_map argument in the function detect_outliers_n_segment
 print("************************ the above is input to forecast segmentation and outlier detection:**************************")
 
 
